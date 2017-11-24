53db7b9a5fbaf4b9285452cf9c200e6d3abc0232
ysjh0014 <33590089+ysjh0014@users.noreply.github.com>
2017-11-24 18:13:47 +0800

    second
    test

---
+++ 401简易网站/jsp/src/gz/itcast/LoginServlet.java
@@ -0,0 +1,55 @@
+package gz.itcast;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		//1.接收参数
+		String userName = request.getParameter("userName");
+		String userPwd = request.getParameter("userPwd");
+		
+		//2.判断逻辑
+		if("401".equals(userName)
+				 && "123456".equals(userPwd)){
+			//重定向
+			response.sendRedirect(request.getContextPath()+"/index.html");
+		}else{
+			
+			//登陆失败，请求重定向
+			response.sendRedirect(request.getContextPath()+"/fail.html");
+		}
+		}
+	
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
